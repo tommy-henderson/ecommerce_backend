71f09e97d1ac4c26d3c5ed9202c20812c02341ab
Tommy Henderson <tommyhenderson@Tommys-MBP.ht.home>
2023-10-29 15:18:48 -0500

    defined columns in category.js file

--- models/Category.js
+++ models/Category.js
@@ -6,7 +6,16 @@ class Category extends Model {}
 
 Category.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primarykey: true,
+      autoincremenet: true
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    }
   },
   {
     sequelize,
@@ -16,5 +25,4 @@ Category.init(
     modelName: 'category',
   }
 );
-
 module.exports = Category;
